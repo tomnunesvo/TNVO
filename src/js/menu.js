b0487808fa5d8150daec87fb7fba75c8fde4a06d
Tom Nunes <tom@tomnunes.com>
2019-04-21 12:50:57 -0400

    clean up menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,61 +4,60 @@ const scrollDownThreshhold = 400
 const scrollUpThreshhold = 300
 const fixedClass = 'menu-fixed'
 const fixedClassIn = 'in'
-const fixedClassActive = 'active'
-let handler = revealMenuHandler
+
+let menuHandler = revealMenuHandler
 let menu
 let scrolling = false;
 
 function revealMenuHandler() {
   if (window.pageYOffset > scrollDownThreshhold) {
-    handler = restoreMenuHandler
     menu.classList.add(fixedClass)
     window.requestAnimationFrame(() => {
       menu.classList.add(fixedClassIn)
     })
+    menuHandler = restoreMenuHandler
   }
 }
 
 function restoreMenuHandler() {
   if (window.pageYOffset < scrollUpThreshhold) {
-    handler = revealMenuHandler
     const onTransitionEnd = () => {
       menu.removeEventListener('transitionend', onTransitionEnd)
-      menu.removeEventListener('webkitTransitionEnd', onTransitionEnd)
+      window.clearTimeout(timeout)
       menu.classList.remove(fixedClass)
     }
+    const timeout = window.setTimeout(onTransitionEnd, 1000)
     menu.addEventListener('transitionend', onTransitionEnd)
-    menu.addEventListener('webkitTransitionEnd', onTransitionEnd)
     menu.classList.remove(fixedClassIn)
+    menuHandler = revealMenuHandler
   }
 }
 
 function run() {
-  handler();
+  menuHandler();
   if (scrolling) {
     window.requestAnimationFrame(run)
   }
-  else { console.log('all done')}
+}
+
+function onScroll() {
+  let timeout = 0
+  return () => {
+    if (scrolling) {
+      window.clearTimeout(timeout)
+    } else {
+      scrolling = true
+      run()
+    }
+    timeout = window.setTimeout(() => scrolling = false, 300)
+  }
 }
 
 whenReady(() => {
-  let timeout = 0
-  if (!document.querySelector('.tnvo-header-cap')) {
-    menu = document.querySelector('.tnvo-header__menu')
-    if (menu) {
-      document.addEventListener('scroll', () => {
-        if (scrolling) {
-          window.clearTimeout(timeout)
-        } else {
-          scrolling = true
-          run()
-        }
-        timeout = window.setTimeout(() => {
-          scrolling = false
-        }, 300)
-      })
-      run()
-    }
+  menu = document.querySelector('.tnvo-header__menu')
+  if (menu && !document.querySelector('.tnvo-header-cap')) {
+    let timeout = 0
+    document.addEventListener('scroll', onScroll())
+    window.setTimeout(run, 600)
   }
 })
-
